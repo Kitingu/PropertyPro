6fb359d40279150d25d35b090da921fcc388152e
benlegendj <benlegendj@gmail.com>
2019-06-15 23:12:20 +0300

    feat(delete advert) -add delete advert form -add js script for modal form -style delete modal form [Finishes #166598108]

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -1,16 +1,19 @@
 document.querySelector('#year').textContent = new Date().getFullYear()
-// const update = document.querySelector('.update')
+const deleteModal = document.querySelector('.delete-modal')
+const deleteSpan = document.querySelector('.close-delete')
 const modal = document.querySelector('.modal')
 const span = document.querySelector('.close')
 
-// update.addEventListener('click', () => {
-//     console.log('clicked')
-//     modal.style.display = "block"
-// })
 
 const update = () => {
     modal.style.display = "block"
 }
+const deleteUpdate = () => deleteModal.style.display = 'block'
+
 span.addEventListener('click', () => {
     modal.style.display = 'none'
+})
+
+deleteSpan.addEventListener('click', () => {
+    deleteModal.style.display = 'none'
 })
