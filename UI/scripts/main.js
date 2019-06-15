861378017ed713ce61705041f80a68d555b419f9
benlegendj <benlegendj@gmail.com>
2019-06-15 14:46:26 +0300

    feature(update property):add update modal form -add update modal form  js script -add style the update modal form -add mark as sold checkbox [finishes #166597987 #166598090]

--- UI/scripts/main.js
+++ UI/scripts/main.js
@@ -1,1 +1,16 @@
 document.querySelector('#year').textContent = new Date().getFullYear()
+// const update = document.querySelector('.update')
+const modal = document.querySelector('.modal')
+const span = document.querySelector('.close')
+
+// update.addEventListener('click', () => {
+//     console.log('clicked')
+//     modal.style.display = "block"
+// })
+
+const update = () => {
+    modal.style.display = "block"
+}
+span.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
