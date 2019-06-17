117a6aa857f0b208ea30062f9fb05b295667a3da
benlegendj <benlegendj@gmail.com>
2019-06-17 22:39:02 +0300

    feat(reset-password) -fix wc validation error -add password reset modal form -distribute javascript code to different files

---
+++ UI/scripts/update.js
@@ -0,0 +1,22 @@
+const deleteModal = document.querySelector('.delete-modal')
+const deleteSpan = document.querySelector('.close-delete')
+
+const modal = document.querySelector('.modal')
+const span = document.querySelector('.close')
+
+
+const update = () => {
+    modal.style.display = "block"
+}
+const deleteUpdate = () => deleteModal.style.display = 'block'
+
+
+
+span.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+
+deleteSpan.addEventListener('click', () => {
+    deleteModal.style.display = 'none'
+})
+
