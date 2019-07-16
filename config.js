42c1e166f0fb3da6a805d693cc730be31fb0df06
benlegendj <benlegendj@gmail.com>
2019-07-16 03:07:30 +0300

    chore(setup database):add database files
    -update sample_env file
    -add drop tables file
    -add create tables file
    -add database querying file
    [delivers #167278120

--- config.js
+++ config.js
@@ -3,19 +3,22 @@ const env = process.env.NODE_ENV  // 'development', 'test','production'
 
 const development = {
     appConfig: {
-        port: 3000
+        port: 3000,
+        DATABASE_URL: process.env.DATABASE_URL
     }
 };
 
 const test = {
     appConfig: {
-        port: 5000
+        port: 5000,
+        DATABASE_URL: process.env.TEST_DATABASE
     }
 };
 
 const production = {
     appConfig: {
-        port: process.env.PORT
+        port: process.env.PORT,
+        DATABASE_URL: process.env.DATABASE_URL
     }
 };
 
